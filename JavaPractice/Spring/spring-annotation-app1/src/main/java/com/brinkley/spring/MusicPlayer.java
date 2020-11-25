e43eef5b530ae1c6fd8489372b5bf32f56de6be9
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-11-25 14:53:35 +0300

    first punch

---
+++ JavaPractice/Spring/spring-annotation-app1/src/main/java/com/brinkley/spring/MusicPlayer.java
@@ -0,0 +1,21 @@
+package com.brinkley.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MusicPlayer {
+    private ClassicalMusic music;
+
+    @Autowired
+    public MusicPlayer(ClassicalMusic music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
